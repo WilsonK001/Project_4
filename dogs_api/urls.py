711a0d3cc621c33d425d0aa8eb4d261d09a2fbf6
Wilson Kameni <wilsonk@wilsons-mbp.myfiosgateway.com>
2021-08-23 15:43:32 -0400

    deploy

--- dogs_api/urls.py
+++ dogs_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpattern = [
+urlpatterns = [
     #  api/contacts will be routed to the dogList view for handling
     path('api/dogs', views.DogList.as_view(), name='dog_list'),
 
